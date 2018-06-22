9c9bd0b96e2fc920b86d203f0b4c6f4785b55737
16110376 <irfan.arianto@students.amikom.ac.id>
2018-06-22 18:37:04 +0700

    new

--- image.php
+++
@@ -1,9 +0,0 @@
-<?php
- if(file_exists($_GET['p'])){
-  $ext = explode('.', $_GET['p']);
-  $ext = end($ext);
-  if($ext=="jpg") $ext="jpeg";
-  header("Content-Type: image/".$ext);
-  echo file_get_contents($_GET['p']);
- }
-?>
