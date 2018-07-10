99ab9e0447355b1a644858fd5189488bc28d9f09
Irfan Arianto <irfan.arianto@students.amikom.ac.id>
2018-07-10 23:49:48 +0700

    updt

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,40 +1,66 @@
- var map;
-      var infowindow;
+$(function () {
 
-      function initMap() {
-        var pyrmont = {lat: -33.867, lng: 151.195};
+    function initMap() {
 
+
+        var pyrmont = {lat: -7.759816, lng: 110.408814};
         map = new google.maps.Map(document.getElementById('map'), {
           center: pyrmont,
-          zoom: 15
+          zoom: 17
         });
 
-        infowindow = new google.maps.InfoWindow();
+        // var location = new google.maps.LatLng(-7.759816, 110.408814);
+
+        // var mapCanvas = document.getElementById('map');
+        // Create the places service.
         var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: pyrmont,
-          radius: 500,
-          type: ['store']
-        }, callback);
+        var getNextPage = null;
+        var moreButton = document.getElementById('more');
+        moreButton.onclick = function() {
+          moreButton.disabled = true;
+          if (getNextPage) getNextPage();
+        };
+
+        // Perform a nearby search.
+        service.nearbySearch(
+            {location: pyrmont, radius: 500, type: ['sport']},
+            function(results, status, pagination) {
+              if (status !== 'OK') return;
+
+              createMarkers(results);
+              moreButton.disabled = !pagination.hasNextPage;
+              getNextPage = pagination.hasNextPage && function() {
+                pagination.nextPage();
+              };
+            });
       }
 
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
+      function createMarkers(places) {
+        var bounds = new google.maps.LatLngBounds();
+        var placesList = document.getElementById('places');
+
+        for (var i = 0, place; place = places[i]; i++) {
+          var image = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+          };
+
+          var marker = new google.maps.Marker({
+            map: map,
+            icon: image,
+            title: place.name,
+            position: place.geometry.location
+          });
+
+          var li = document.createElement('li');
+          li.textContent = place.name;
+          placesList.appendChild(li);
+
+          bounds.extend(place.geometry.location);
         }
+        map.fitBounds(bounds);
       }
-
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-      }
+});
